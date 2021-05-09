6d92c3b5f206b1097d60746d0f715d39198ee4ef
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-05-10 01:08:44 +0530

    notify on block action

--- src/blockAction.ts
+++ src/blockAction.ts
@@ -3,38 +3,42 @@ import {
   IModify,
   IPersistence,
   IRead,
-} from '@rocket.chat/apps-engine/definition/accessors';
+} from "@rocket.chat/apps-engine/definition/accessors";
 import {
   IUIKitResponse,
   UIKitBlockInteractionContext,
-} from '@rocket.chat/apps-engine/definition/uikit';
+} from "@rocket.chat/apps-engine/definition/uikit";
 
-import { shortenBlockMessage } from './utils/enums';
-import getYourlsStats from './lib/yourls/stats';
-import createStatsModal from './lib/modals/createStatsModal';
+import { shortenBlockMessage } from "./utils/enums";
+import getYourlsStats from "./lib/yourls/stats";
+import createStatsModal from "./lib/modals/createStatsModal";
+import sendNotifyMessage from "./lib/sendInChat/sendNotifyMessage";
 
 export default async function handleBlockAction(
   ctx: UIKitBlockInteractionContext,
   read: IRead,
   http: IHttp,
   _persist: IPersistence,
-  modify: IModify,
+  modify: IModify
 ): Promise<IUIKitResponse> {
-  const {
-    actionId, value, triggerId, user,
-  } = ctx.getInteractionData();
+  const { actionId, value, triggerId, user, room } = ctx.getInteractionData();
 
-  if (actionId === shortenBlockMessage.stats && value) {
+  if (actionId === shortenBlockMessage.stats && value && room) {
     const result = await getYourlsStats(http, value);
 
-    if ('name' in result) {
+    if ("name" in result) {
       const modal = createStatsModal(
         modify.getCreator().getBlockBuilder(),
-        result,
+        result
       );
       await modify.getUiController().openModalView(modal, { triggerId }, user);
     } else {
-      // TODO: send **notify** message for error
+      sendNotifyMessage({
+        notify: modify.getNotifier(),
+        sender: user,
+        msg: result.message,
+        room,
+      });
     }
   }
 
